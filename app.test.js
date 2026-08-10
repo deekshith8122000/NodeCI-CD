59f271f217c4fade4198946638e181e6d617f94b
deekshith8122000 <deekshithssuvarna27@gmail.com>
2026-08-10 14:17:36 +0530

    Test failing corrected

--- app.test.js
+++ app.test.js
@@ -2,11 +2,11 @@ const request = require("supertest");
 const app = require("./app");
 
 describe("GET /hello", () => {
-    test("should return Hello World", async () => {
+    test("should return Hello Javascript", async () => {
         const response = await request(app).get("/hello");
 
         expect(response.statusCode).toBe(200);
-        expect(response.body.message).toBe("Hello World");
+        expect(response.body.message).toBe("Hello Javascript");
         // expect(response.body.message).toBe("Hello Universe");
     });
 });
